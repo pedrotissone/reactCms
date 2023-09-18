8736c27d317b207fe5dbfdfb1cb2655d642cfd37
Pedro Tissone <pedrotissone@gmail.com>
2023-09-18 05:38:09 -0300

    footer migrado a mui

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./footer.css"
 import { BiLogoInstagramAlt, BiSolidMap, BiSolidCopyright } from "react-icons/bi"
 import { FaWhatsapp } from "react-icons/fa"
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import PlaceIcon from '@mui/icons-material/Place';
+import { Box, Stack } from "@mui/material";
 
 function Footer() {
 
@@ -15,14 +19,14 @@ function Footer() {
 
     return(
         <>
-            <div className="footerTextDiv">
+            {/* <div className="footerTextDiv">
                 <div className="react-icons">
                     < BiSolidMap  size={17} style={{color:"whitesmoke"}}/>
                 </div>
             <p className="footerText">Ciudad Autónoma de Buenos Aires, Argentina</p>
             </div>            
 
-            <div className="footerTextDiv">
+            <div className="footerTextDiv">                
             < FaWhatsapp size={17} style={{color:"whitesmoke"}}/>
             <p className="footerText" onClick={handleClickWhatsapp}>+54 9 11 6356-6348</p>
             </div>
@@ -34,7 +38,31 @@ function Footer() {
             <div className="footerTextDiv">
             <p className="footerText">React E-Commerce Copyright 2023</p>
             < BiSolidCopyright size={17} style={{color:"whitesmoke"}}/>
-            </div>
+            </div> */}
+
+            <Stack direction={{xl:"row", lg:"row"}} justifyContent="space-around" alignItems="center">
+
+                <Box sx={{display:"flex", alignItems: "flex-end"}} marginBottom={2} marginTop={2}>
+                <PlaceIcon sx={{color:"whitesmoke"}} fontSize="small"/>
+                <span className="footerText">Ciudad Autónoma de Buenos Aires, Argentina</span>
+                </Box>
+
+                <Box sx={{display:"flex", alignItems: "flex-end"}}marginBottom={2} marginTop={2}>
+                <WhatsAppIcon sx={{color:"whitesmoke"}} fontSize="small"/>
+                <span className="footerText" onClick={handleClickWhatsapp}>+54 9 11 6356-6348</span>
+                </Box>
+
+                <Box sx={{display:"flex", alignItems: "flex-end"}}marginBottom={2} marginTop={2}>
+                <InstagramIcon sx={{color:"whitesmoke"}} fontSize="small"/>
+                <span className="footerText" onClick={handleClickInstagram}>@Tiza90</span>
+                </Box>
+
+                <Box sx={{display:"flex", alignItems: "flex-end"}}marginBottom={2} marginTop={2}>
+                < BiSolidCopyright size={19} style={{color:"whitesmoke"}}/>
+                <span className="footerText">React E-Commerce Copyright 2023</span>
+                </Box>
+
+            </Stack>
             
         </>
     )
--- src/componentes/NavBar/NavBarContainer.jsx
+++ src/componentes/NavBar/NavBarContainer.jsx
@@ -3,7 +3,7 @@ import "./navBarContainer.css"
 import {Link} from "react-router-dom"
 import {cartContext} from "../../context/cartContext"
 import { useContext } from "react"
-import { Stack } from "@mui/material"
+import { ImageList, Stack } from "@mui/material"
 
 
 
@@ -13,7 +13,7 @@ function NavBarContainer(props) {
 
   return (
     <>    
-       <div className="navBar">
+       {/* <div className="navBar">
 
         <Link className="navBarLink1" to="/category/Flores preservadas" ><NavBarItem title="CATEGORY 1" img="/img/categoryZapatillas.jpg"/></Link>
 
@@ -26,11 +26,23 @@ function NavBarContainer(props) {
       {user && <div className='divCrearNuevoProducto'>
       <Link to={"/CrearNuevoProducto"}><h2 className="crearNuevoProducto">Crear nuevo producto</h2></Link>
       <Link to={"/ModificarPrecioDeProductos"}><h2 className="modificarPrecioDeProductos">Modificar precio de productos</h2></Link>
+      </div>} */}
+
+      <Stack sx={{backgroundColor: "rgb(153 148 148)", mb: "2rem"}} direction={{sm:"row", md:"row", lg:"row", xl:"row"}} justifyContent={{xs: "center", sm:"space-around", md: "space-around", lg:"space-around", xl: "space-around"}} alignItems="center" >
+
+        <Link className="navBarLink1" to="/category/Flores preservadas" ><NavBarItem title="CATEGORY 1" img="/img/categoryZapatillas.jpg"/></Link>
+
+        <Link className="navBarLink2" to="/category/Piedras"><NavBarItem title="CATEGORY 2" img="/img/categoryGorras.jpg"/></Link>
+
+        <Link className="navBarLink3" to="/category/Porcelana fria"><NavBarItem title="CATEGORY 3" img="/img/categoryAnteojos.webp"/></Link>
+
+      </Stack>
+
+      {user && <div className='divCrearNuevoProducto'>
+      <Link to={"/CrearNuevoProducto"}><h2 className="crearNuevoProducto">Crear nuevo producto</h2></Link>
+      <Link to={"/ModificarPrecioDeProductos"}><h2 className="modificarPrecioDeProductos">Modificar precio de productos</h2></Link>
       </div>}
 
-      <Stack>
-      
-      </Stack>      
     </>
   )
 }
